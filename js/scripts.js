d30c6bd212570f78da8c14d29da6bfd3f8e3cbe4
Keagan <kberstler2012@eagles.ewu.edu>
2018-11-01 10:50:02 -0700

    Added Second city
    added Hilo, Hawaii

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
+// City One - Cheney
 $.simpleWeather({
-    location: 99004,
-    success: function(weather) {
+  location: 99004,
+  success: function(weather) {
+     
+     $('#city-one h2').text(weather.city);
+     $('#city-one .image img').attr( 'src', weather.image  );
+     $('#city-one .temp').text(weather.temp);
+     $('#city-one .time').text(weather.updated);
+  
+  }, 
+  error: function(error) {
+    console.log('Go outside!');
+  }
+});
 
-      console.log(weather.image);
-      console.log(weather.city);
-      console.log(weather.temp);
-      console.log(weather.updated);
-
-      $('.image img').attr('src', weather.image);
-      $('.city').text(weather.city);
-      $('.temp').text(weather.temp);
-      $('.time').text(weather.updated);
-
-
-    }, 
-    error: function(error) {
-      console.log('Go outside!');
-    }
- });
+// City Two - Hilo
+$.simpleWeather({
+  location: 96720,
+  success: function(weather) {
+     
+     $('#city-two h2').text(weather.city);
+     $('#city-two .image img').attr( 'src', weather.image  );
+     $('#city-two .temp').text(weather.temp);
+     $('#city-two .time').text(weather.updated);
+  
+  }, 
+  error: function(error) {
+    console.log('Go outside!');
+  }
+});
